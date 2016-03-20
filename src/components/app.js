bd00418b9ffa55df2b91b2587d5e39847fbb99ca
Francisco Manuel Olmedo Bueno <Francisco Manuel Olmedo Bueno>
2016-03-20 20:25:24 +0100

    Added basic functionality

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Component } from 'react';
 
+import SearchBar from '../containers/search-bar';
+import WeatherList from '../containers/weather-list';
+
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <SearchBar />
+        <WeatherList />
+      </div>
     );
   }
 }
